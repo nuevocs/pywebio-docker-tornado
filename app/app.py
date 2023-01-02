ec774ffadf0ecbb96b9138850b87d9d2c35b8f00
Tatsuya Tomita <tat@seriousexplosion.net>
2023-01-02 17:41:57 +0900

    init

---
+++ app/app.py
@@ -0,0 +1,28 @@
+import tornado.ioloop
+import tornado.web
+from pywebio.platform.tornado import webio_handler
+from pywebio.output import *
+from pywebio import STATIC_PATH
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+
+def task_func() -> None:
+    put_markdown("""
+        # Year!!!!
+        - 1
+        - 2
+        - 3
+    """)
+
+
+if __name__ == "__main__":
+    application = tornado.web.Application([
+        (r"/", MainHandler),
+        (r"/tool", webio_handler(task_func)),  # `task_func` is PyWebIO task function
+    ])
+    application.listen(port=8080, address='localhost') # run python3 app.py
+    tornado.ioloop.IOLoop.current().start()
